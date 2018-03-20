44367f072a0f4216d4d9558ee02d1ae7a6f9f58f
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-03-20 21:00:36 +0700

    Add webpack config with {typescript, scss}

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,48 @@
 const path = require('path');
+const __srcdir = path.join(__dirname, 'src');
 
-const config = {
-
+module.exports = {
+  entry: [
+    path.join(__srcdir, 'ts', 'index.ts'),
+    path.join(__srcdir, 'scss', 'main.scss')
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: 'assets'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.scss']
+  },
+  devtool: 'source-map',
+  plugins: []
 };
-
-module.exports = config;
